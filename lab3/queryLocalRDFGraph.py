8029c8c32d6591bcba25ecec01f6d26446f8eae4
Fei Phoon <fei.phoon@gmail.com>
2021-02-10 11:37:56 +0000

    Update queryLocalRDFGraph.py

--- lab3/queryLocalRDFGraph.py
+++ lab3/queryLocalRDFGraph.py
@@ -13,13 +13,11 @@ def queryLocalGraph():
     g.parse("playground.ttl", format="ttl")
   
     
-    
     print("Loaded '" + str(len(g)) + "' triples.")
     
     #for s, p, o in g:
     #    print((s.n3(), p.n3(), o.n3()))
     
-        
     print("Females:")
     
     qres = g.query(
@@ -29,8 +27,29 @@ def queryLocalGraph():
     }""")
 
     for row in qres:
-        #Row is a list of matched RDF terms: URIs, literals or blank nodes
-        print("%s is female with name '%s'" % (str(row.thing),str(row.name)))
+      #Row is a list of matched RDF terms: URIs, literals or blank nodes
+      print("%s is female with name '%s'" % (str(row.thing),str(row.name)))
+
+    # Query 1
+    qres_grandfather1 = g.query(
+      """select ?name ?grandfather where {
+        ttr:Eve dbo:parent / dbo:parent ?grandfather  .
+      }
+      """
+    )
+
+    qres_grandfather2 = g.query(
+      """select ?name ?grandfather where {
+        ttr:Eve dbo:parent [dbo:parent ?grandfather]  .
+      }
+      """
+    )
+
+    for row in qres_grandfather:
+      print(f"{row.grandfather}: {row.name}")
+
+    # Query 2
+
         
         
 queryLocalGraph()
